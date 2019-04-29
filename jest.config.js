c2af516a40e447b5bb370b000a0a10a6a4535647
lywTYH <763613923@qq.com>
2019-04-29 15:07:54 +0800

    adjust docker jest speed

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  roots: ['./src'],
   verbose: false,
   collectCoverage: false,
   coverageDirectory: './coverage/',
-  roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testEnvironment: 'node',
 };
